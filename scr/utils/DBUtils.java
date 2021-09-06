9636e085f91dd027edf9d491d13f6342645677ae
thong nguyen <71358789+BinBeo3007@users.noreply.github.com>
2021-09-06 13:57:02 +0700

    Update DBUtils.java

--- scr/utils/DBUtils.java
+++ scr/utils/DBUtils.java
@@ -9,36 +9,28 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class DBUtils {
- 
- private String db_url = null;
- private String db_user = null;
- private String db_password = null;
- private String db_driver = null;
+
  private Connection con;
  
- public Connection getConnection(){
-	 Properties p = new Properties();
-	 try {
-		 p.load(new FileInputStream("database.properties"));
-		 db_url = p.getProperty("db_url");
-		 db_user = p.getProperty("db_user");
-		 db_password = p.getProperty("db_password");
-		 db_driver = p.getProperty("db_driver");
-		 Class.forName(db_driver);
-		 con = DriverManager.getConnection(db_url, db_user, db_password);
-	 } catch (FileNotFoundException e) {
-	 System.err.println("Không thấy tệp tín database.properties.");
-	 } catch (IOException e) {
-	 System.err.println("Đọc tệp tin có lỗi.");
-	 } catch (ClassNotFoundException e) {
-	 System.err.println("Không tìm thấy class.");
-	 } catch (SQLException e) {
-	 System.err.println("Lỗi kết nối cơ sở dư liệu.");
-	 }
-	 if (con == null) {
-	 throw new NullPointerException("Kết nối không thành công.");
-	 }
-
-  return con;
+ public DBUtils() {
+     con = getConnection();
  }
-} 
+ 
+ public Connection getConnection(){
+	 String status="";
+	 try {
+         String url = "jdbc:mysql://localhost:3306/qlmb";
+         Properties props = new Properties();
+         props.setProperty("user","root");
+         props.setProperty("password","");
+         props.setProperty("ssl","false");
+         con = DriverManager.getConnection(url, props);
+         status= "Connect successfully";
+     } catch (Exception e) {
+         status= "Connect failure";
+         e.printStackTrace();
+     }
+	 System.out.print(status);
+  return con;
+ } 
+}
